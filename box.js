5c74a22f0afe457029071109dc7d0d834ff1e57d
Bart De Meyere <141313819+BartDeMeyere@users.noreply.github.com>
2024-09-10 22:32:53 +0200

    Add files via upload

---
+++ box.js
@@ -0,0 +1,32 @@
+class Box{
+
+    constructor(x , y , size , color){
+
+        this.x = x 
+        this.y = y 
+        this.size = size
+        this.finished = false
+        this.color = color
+        this.currentsize = 0
+        this.targetx = undefined 
+        this.targety = undefined
+    }
+
+    draw(){
+
+
+        c.beginPath()
+        c.strokeStyle = this.color
+        c.rect(this.x , this.y , this.size , this.size)
+        c.stroke()
+        c.closePath()
+      
+    }
+
+    update(){
+
+        this.x += (this.targetx - this.x) * .03
+        this.y += (this.targety - this.y) * .03
+    }
+
+}
---
+++ index.js
@@ -0,0 +1,102 @@
+var canvas = document.querySelector("canvas")
+var c = canvas.getContext("2d")
+
+canvas.width = innerWidth * devicePixelRatio 
+canvas.height = innerHeight * devicePixelRatio
+
+canvas.style.width = 100 + "%"
+canvas.style.height = 100 + "%"
+
+var size = 600 
+var blocks = []
+var color = "rgba(255,255,255,.9)"
+var order = 1
+var animId = undefined
+
+function renderContent(){
+
+    c.clearRect(0 , 0 , canvas.width , canvas.height)
+
+    blocks.forEach(box => {
+
+        box.draw()
+        box.update()
+
+    })
+
+    animId = requestAnimationFrame(renderContent)
+
+}
+
+function RenderFractal(){
+
+    var newblocks = []
+
+    if(order > 6){
+
+        cancelAnimationFrame(animId)
+        return
+    }
+
+    if(order === 1){
+
+       var newblock = new Box(canvas.width/2 - size/2  , canvas.height/2 - size/2, size , color)
+       newblock.targetx = canvas.width/2 - size/2
+       newblock.targety = canvas.height/2 - size/2
+       blocks.push(newblock)
+
+    }
+
+    if(order > 1){
+
+        for(var i = 0 ; i < blocks.length ; i++){
+
+            //for each block that is finished create 8 new blocks
+            if(!blocks[i].finished){
+
+                var newsize = blocks[i].size/3
+                var sx = blocks[i].x + newsize
+                var sy = blocks[i].y + newsize
+
+                for(var row = -1 ; row < 2 ; row++){
+
+                    for(var col = -1 ; col < 2 ; col++){
+
+                        if(row !== 0 || col !== 0){
+
+                            var newblock = new Box(blocks[i].x + newsize + col * newsize/3 , blocks[i].y + newsize + row * newsize/3 , newsize , color)
+                            newblock.targetx = sx + col * blocks[i].size 
+                            newblock.targety = sy + row * blocks[i].size 
+                            newblocks.push(newblock)
+                        }
+                    }
+                }
+                 
+            }
+
+            blocks[i].finished = true
+
+            //newblocks.push(blocks[i])
+        }
+
+        blocks = newblocks
+    }
+
+    order++
+
+    setTimeout(RenderFractal,3000)
+
+
+}
+
+renderContent()
+RenderFractal()
+
+
+
+
+
+
+
+
+
